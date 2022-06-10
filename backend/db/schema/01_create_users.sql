d872d2cbbecbc9fe328e6f9982a9c21c28cc6c95
JC Flores <tothenextcode@gmail.com>
2022-06-10 16:57:01 -0700

    Modify user schema according to ERD design

--- backend/db/schema/01_create_users.sql
+++ backend/db/schema/01_create_users.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS users CASCADE;
 -- CREATE USERS
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255)
+  username VARCHAR(255) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password_digest VARCHAR(100) NOT NULL,
+  avatar_id integer
 );
