e85e1058f337d8a7e479bdf13e0a591f8094ec4e
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-10 18:47:18 -0500

    add test component for nes.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,16 @@ import Button from './components/Button';
 import Card from './components/Card';
 import Bunny from './components/Bunny';
 
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Bunny />} />
-        <Route path="button" element={<><Button /><Outlet /></>}>
+        <Route path="/" element={<Card />} />
+        {/* <Route path="button" element={<><Button /><Outlet /></>}>
           <Route path="card" element={<Card />} />
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,6 +1,13 @@
 const Card = () => {
-  return(
-    <h1>Card</h1>
+  return (
+    <>
+      <h1>Card</h1>
+
+      <div className="nes-container with-title is-centered">
+        <p className="title">Container.is-centered</p>
+        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+      </div>
+    </>
   );
 };
 
