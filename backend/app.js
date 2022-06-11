86d0b84b4e3fd70899203e5e956db03d5ac9802b
Muhammed Tariq <momotrq94@gmail.com>
2022-06-10 21:59:09 -0400

    create cards router

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const db = require('./configs/db.config');
+const db = require("./configs/db.config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var cardsRouter = require("./routes/cards");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter(db));
-app.use('/cats', usersRouter(db));
+app.use("/", indexRouter);
+app.use("/users", usersRouter(db));
+app.use("/cats", usersRouter(db));
+app.use("/cards", cardsRouter(db));
 
 module.exports = app;
