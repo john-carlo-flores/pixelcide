6593edf2bb6e4fb8d2715309b97f0cf0b1915039
Muhammed Tariq <momotrq94@gmail.com>
2022-06-11 14:24:21 -0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import './App.scss';
+import "./App.scss";
 
-import Button from './components/Button';
-import Card from './components/Card';
-import Bunny from './components/Bunny';
-import Homepage from './routes/Homepage';
+import Button from "./components/Button";
+import Bunny from "./components/Bunny";
+import Homepage from "./routes/Homepage";
 
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
   return (
