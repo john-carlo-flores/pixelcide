673ff37d4702a75fb99e1160b57f5f533762ee53
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 17:38:42 -0500

    add logic to setup castle and tavern decks after axios request

--- frontend/src/components/Game/Deck.jsx
+++ frontend/src/components/Game/Deck.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 import Card from '../Game/Card';
+import emptyCard from '../../assets/cards/placeholder.png';
 
-const Deck = ({ deck }) => {
+const Deck = (props) => {
+  const { deck, name, setCurrentBoss } = props;
+
+  const lastDiscardCard = deck.length > 0 && name === 'discard' ? deck.at(-1) : {};
+  const lastCastleCard = deck.length > 0 && name === 'castle' ? deck.at(-1) : {};
+  const lastTavernCard = deck.length > 0 && name === 'tavern' ? deck.at(-1) : {};
+
+  useEffect(() => {
+    name === 'castle' && setCurrentBoss(lastCastleCard);
+  }, [lastCastleCard]);
+
   return (
     <div className="Deck">
-      {deck.map((card) => (
-        <Card id={card.id} name={card.card_name} key={card.id} image_front={card.image_front} image_back={card.image_back} />
-      ))}
+      {/* discard deck */}
+      {name === 'discard' && !deck.length ? <Card image={emptyCard} /> : <Card image={lastDiscardCard.image_front} />}
+
+      {/* castle deck */}
+      {name === 'castle' && <Card image={lastCastleCard.image_front} />}
+
+      {/* tavern deck */}
+      {name === 'tavern' && <Card image={lastTavernCard.image_back} />}
     </div>
   );
 };
