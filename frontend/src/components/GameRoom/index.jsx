b90f7d9e877d92802941b6e5f7008917085199bf
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 17:37:13 -0500

    import and render Game component

--- frontend/src/components/GameRoom/index.jsx
+++ frontend/src/components/GameRoom/index.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Game from '../Game';
 
 const GameRoom = () => {
-  return(
+  return (
     <>
       <h1>This is the in-game lobby where users can join and wait for host to start.</h1>
       <Link to="/">Back to home page</Link>
+      <Game />
     </>
   );
 };
