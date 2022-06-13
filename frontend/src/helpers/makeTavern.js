6e85202d21f9f2e4bfcbafab3eb916bba6c964cd
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 15:19:58 -0500

    add makeTavern as helper fn

---
+++ frontend/src/helpers/makeTavern.js
@@ -0,0 +1,9 @@
+import shuffle from './shuffle';
+
+const makeTavern = (cardsArray) => {
+  const finalDeck = cardsArray.filter((card) => card.tag !== 'K' && card.tag !== 'Q' && card.tag !== 'J');
+  shuffle(finalDeck);
+  return finalDeck;
+};
+
+export default makeTavern;
