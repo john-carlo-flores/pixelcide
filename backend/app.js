aa0281c091098575fc06cd78a4a88f7d9d3fb6b6
JC Flores <tothenextcode@gmail.com>
2022-06-13 12:42:26 -0700

    Modify index router to accept db parameter

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter(db));
 app.use('/users', usersRouter(db));
 app.use('/games', gamesRouter(db));
 app.use('/cards', cardsRouter(db));
