1475ffff277be9c076aa9da94ad22a4735372885
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 18:41:52 -0500

    add styles for DeckList component

--- frontend/src/components/Game/DeckList.jsx
+++ frontend/src/components/Game/DeckList.jsx
@@ -1,11 +1,12 @@
 import Deck from './Deck';
+import '../../styles/DeckList.scss';
 
 const DeckList = (props) => {
   const { tavern, discard, castle, setCurrentBoss } = props;
   return (
     <div className="DeckList">
-      {<Deck deck={tavern} name={'tavern'} />}
       {<Deck deck={discard} name={'discard'} />}
+      {<Deck deck={tavern} name={'tavern'} />}
       {<Deck deck={castle} name={'castle'} setCurrentBoss={setCurrentBoss} />}
     </div>
   );
