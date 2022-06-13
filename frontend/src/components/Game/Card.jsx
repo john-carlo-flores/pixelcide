649f90d935d7e33221e40b020d835a32503abf54
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 14:07:29 -0500

    refactor card to render all cards as images

--- frontend/src/components/Game/Card.jsx
+++ frontend/src/components/Game/Card.jsx
@@ -1,11 +1,7 @@
 import '../../styles/Card.scss';
-import queen from '../../assets/sprites/animated-sprites/queen/queen-diamonds.gif';
 
-const animatedCards = [1, 2, 3, 4, 50, 51, 52, 54, 53, 41, 42, 43, 44, 45, 46, 47, 48];
-
-const Card = ({ image_front, id }) => {
-  // return <div className="Card">{!animatedCards.includes(id) && <img src={image_front} alt="" />}</div>;
-  return <img src={queen} alt="" />;
+const Card = ({ image_front, id, card_name }) => {
+  return <img src={image_front} alt={card_name} />;
 };
 
 export default Card;
