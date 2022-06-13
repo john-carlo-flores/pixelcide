a09464e8b7d40120a8184b65aa8dfd04d2958d75
JC Flores <tothenextcode@gmail.com>
2022-06-13 13:55:23 -0700

    Implement authentication helper functions to generate and verify jsonwebtokens

---
+++ backend/helpers/authentication.js
@@ -0,0 +1,35 @@
+const { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, SALT } = process.env;
+
+export const generateAccessToken = (id) => {
+  return jwt.sign(
+    { id }, 
+    ACCESS_TOKEN_KEY,
+    { expiresIn: "6h"}
+    );
+};
+
+export const generateRefreshToken = (id) => {
+  return jwt.sign(
+    { id }, 
+    REFRESH_TOKEN_KEY,
+    );
+};
+
+export const verify = (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, ACCESS_TOKEN_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid!");
+      }
+
+      req.user = user;
+      next();
+    })
+
+  }
+    res.status(401).json("User failed authentication!");
+};
