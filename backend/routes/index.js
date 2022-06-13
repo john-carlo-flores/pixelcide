82d1cd228f571ef5b5ad3537a4850e64c07d0cf0
JC Flores <tothenextcode@gmail.com>
2022-06-13 12:43:30 -0700

    Setp post login request. Still need to add jwt and return corresponding data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+module.exports = (db) => {
+  /* GET home page. */
+  router.get('/', (req, res) => {
+    res.render('index', { title: 'Welcome to Pixelcide API' });
+  });
+
+  router.post('/login', (req, res) => {
+    const { username, password } = req.body.user;
+    
+    const command = `
+      SELECT * FROM USERS WHERE username = $1
+    `;
+
+    db.query(command, [username])
+      .then(data => {
+        const { id, username, name, password_digest, avatar_id } = data.rows[0];
+
+        res.send(`Success! User ID is ${id}`);
+      })
+      .catch(err => {
+        res.status(400).json("Username and/or password is incorrect");
+      })
+  });
+
+  return router;
+};
+
