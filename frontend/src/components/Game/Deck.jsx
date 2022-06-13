75418bbad70bef99586a7bc885514d5d218525af
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 15:25:32 -0500

    add useEffect to render castleDeck and tavernDeck on recieving cards

--- frontend/src/components/Game/Deck.jsx
+++ frontend/src/components/Game/Deck.jsx
@@ -1,11 +1,11 @@
-import Card from './Card';
+import Card from '../Game/Card';
 
 const Deck = ({ deck }) => {
   return (
     <div className="Deck">
-      {/* {data.map((card) => (
+      {deck.map((card) => (
         <Card id={card.id} name={card.card_name} key={card.id} image_front={card.image_front} image_back={card.image_back} />
-      ))} */}
+      ))}
     </div>
   );
 };
