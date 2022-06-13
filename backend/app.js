09af133ae6d0b45e8032ce3488aafacdffa88be9
JC Flores <tothenextcode@gmail.com>
2022-06-13 13:02:49 -0700

    Install jsonwebtoken and reference in app.js and index router

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const db = require('./configs/db.config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var gamesRouter = require('./routes/games');
-var cardsRouter = require('./routes/cards');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const cardsRouter = require('./routes/cards');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
@@ -20,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter(db));
+app.use('/', indexRouter(db, jwt));
 app.use('/users', usersRouter(db));
 app.use('/games', gamesRouter(db));
 app.use('/cards', cardsRouter(db));
