4dca54f600f5e56423a1e136a97fe6e28307a644
Muhammed Tariq <momotrq94@gmail.com>
2022-06-13 12:56:22 -0400

    create users post request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,39 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 module.exports = (db) => {
   // all routes will go here
-  router.get('/', (req, res) => {
+  router.get("/", (req, res) => {
     const command = "SELECT * FROM users";
-    db.query(command).then(data => {
+    db.query(command).then((data) => {
       res.json(data.rows);
-    })
+    });
+  });
+
+  router.post("/", (req, res) => {
+    console.log("entered post request");
+    console.log(req.body);
+    const { user } = req.body;
+
+    console.log(user);
+
+    const { username, name, email, password_digest, avatar_id } = user;
+    const command = `
+    INSERT INTO users (username, name, email, password_digest, avatar_id)
+    VALUES
+    ($1, $2, $3, $4, $5);
+    `;
+    db.query(command, [username, name, email, password_digest, avatar_id])
+      .then((data) => {
+        res.send("User Saved");
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+
+  router.get("/haha", (req, res) => {
+    console.log("we are in haha");
   });
 
   return router;
-}
+};
