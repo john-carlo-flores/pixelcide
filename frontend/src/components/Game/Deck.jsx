7dfa26c62ab3537d7a52674c00214617cdb62ca2
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 14:32:59 -0500

    move axios request fron Deck to Game

--- frontend/src/components/Game/Deck.jsx
+++ frontend/src/components/Game/Deck.jsx
@@ -1,22 +1,11 @@
 import Card from './Card';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const baseURL = 'http://localhost:8080/cards';
 
 const Deck = ({ deck }) => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setData(response.data);
-    });
-  }, []);
-
   return (
     <div className="Deck">
-      {data.map((card) => (
+      {/* {data.map((card) => (
         <Card id={card.id} name={card.card_name} key={card.id} image_front={card.image_front} image_back={card.image_back} />
-      ))}
+      ))} */}
     </div>
   );
 };
--- frontend/src/components/Game/index.jsx
+++ frontend/src/components/Game/index.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
 import PlayerList from './PlayerList';
 import DeckList from './DeckList';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:8080/cards';
+
 const Game = () => {
+  const [cards, setCards] = useState([]);
   const [discard, setDiscard] = useState([]);
   const [castle, setCastle] = useState([]);
   const [tavern, setTavern] = useState([]);
@@ -10,9 +15,15 @@ const Game = () => {
   const [currentPlayer, setCurrentPlayer] = useState();
   const [currentBoss, setCurrentBoss] = useState();
 
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setCards(response.data);
+    });
+  }, []);
+
   return (
     <div className="Game">
-      <PlayerList players={players} currentPlayer={currentPlayer} />
+      {/* <PlayerList players={players} currentPlayer={currentPlayer} />*/}
       <DeckList tavern={tavern} discard={discard} castle={castle} />
     </div>
   );
