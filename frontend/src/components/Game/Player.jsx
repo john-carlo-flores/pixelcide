25a59090e175b970cc609d6c71bf4d197ab72161
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-13 19:52:02 -0500

    add player-hand on first card deal

--- frontend/src/components/Game/Player.jsx
+++ frontend/src/components/Game/Player.jsx
@@ -1,5 +1,17 @@
-const Player = ({ currentPlayer }) => {
-  return <div className="Player">player</div>;
+import Card from './Card';
+
+const Player = (props) => {
+  const { hand, playerName } = props;
+  return (
+    <div className="Player">
+      <div className="player-cards">
+        {hand.map((card) => (
+          <Card key={card.id} image={card.image_front} />
+        ))}
+      </div>
+      <div className="player-name">{playerName}</div>
+    </div>
+  );
 };
 
 export default Player;
