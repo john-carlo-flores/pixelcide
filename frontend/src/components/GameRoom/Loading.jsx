54f88626ebc5b8cb0f69e04967a6f26c281e521b
JC Flores <tothenextcode@gmail.com>
2022-06-14 12:00:53 -0700

    Implement Loading component with animations

---
+++ frontend/src/components/GameRoom/Loading.jsx
@@ -0,0 +1,38 @@
+import loading from '../../assets/card-spinner.gif';
+
+import styles from '../../styles/GameRoom/Loading.module.scss';
+
+import { useEffect, useState } from 'react';
+
+const Loading = () => {
+  const [dots, setDots] = useState('Loading Game');
+
+  const cycleDots = () => {
+    setDots(prev => {
+      console.log('prev', prev);
+      if (prev.length < 15) {
+        return `${prev}.`;
+      }
+
+      return 'Loading Game';
+    });
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      cycleDots();
+    }, 500);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className={styles.loading}>
+      <img className={styles.img} src={loading} alt='Loading'/>
+      <span className={styles.loadingText}>{dots}</span>
+    </div>
+  );
+};
+
+export default Loading;
+
