3f2330d3eb40d7c0d7e08626d847269de9b93fd0
JC Flores <tothenextcode@gmail.com>
2022-06-13 20:32:24 -0700

    Reference jwt-decode and implement refreshToken to obtain new access and refreshToken on token expiry. Create new axiosJWT instance and intercept on logout and when token expires

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 const baseURL = 'http://localhost:8080';
 
 const useAuth = (initial) => {
   const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')));
+  const axiosJWT = axios.create();
 
   const verifyLogin = (username, password) => {
     if (username && password) {
@@ -25,8 +27,9 @@ const useAuth = (initial) => {
     return false;
   };
 
+
   const logout = () => {
-    axios.post(`${baseURL}/logout`, { token: user.refreshToken }, {
+    axiosJWT.post(`${baseURL}/logout`, { token: user.refreshToken }, {
       headers: { Authorization: `Bearer ${user.accessToken}` }
     })
       .then(response => {
@@ -34,6 +37,37 @@ const useAuth = (initial) => {
         setUser(null);
       });
   };
+
+  const refreshToken = () => {
+    return axios.post(`${baseURL}/refresh`, { token: user.refreshToken })
+      .then(res => {
+        const refreshUser = {
+          ...user,
+          accessToken: res.data.accessToken,
+          refreshToken: res.data.refreshToken
+        };
+
+        setUser(refreshUser);
+        sessionStorage.setItem('user', JSON.stringify(refreshUser))
+      });
+  };
+
+  axiosJWT.interceptors.request.use(config => {
+    const currentDate = new Date();
+      const decodeToken = jwtDecode(user.accessToken);
+
+      // If token has expired
+      if(decodeToken.exp * 1000 < currentDate.getTime()) {
+        refreshToken().then(data => {
+          config.headers.authorization = `Bearer ${data.accessToken}`;
+        }).catch(err => {
+          return Promise.reject(err);
+        });
+      }
+
+      return config;
+    }
+  );
 
   return { user, verifyLogin, logout };
 };
