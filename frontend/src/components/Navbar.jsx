53257bf7527bd88aa529bb8f2d6801bfccf5e0ac
JC Flores <tothenextcode@gmail.com>
2022-06-13 19:48:31 -0700

    Pass down verifylogin and logout to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,47 @@
 import Button from "../components/Button";
+import Login from "../components/Authentication/Login";
+
 import "../styles/Navbar.scss";
 
-export default function Navbar(props) {
-  return (
-    <nav className="navbar">
-      <a
-        className="how-to-play"
-        href="https://www.badgersfrommars.com/assets/RegicideRulesA4.pdf"
-        target="_blank"
-      >
-        How to Play
-      </a>
+import { useState } from "react";
 
-      <div className="navbar-right">
-        {!props.user && (
-          <p onClick={props.onClick} className="login nes-pointer">
-            Login
-          </p>
-        )}
-        {props.user && (
-          <div className="user-logout">
-            <p>Welcome {props.user} |</p>
-            <p className="logout nes-pointer" onClick={props.onClick}>
-              &nbsp;Logout
+export default function Navbar(props) {
+  const [login, setLogin] = useState(false);
+
+  const { userAuth, logout } = props;
+
+  const toggleLoginForm = () => {
+    setLogin(prev => !prev);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <a
+          className="how-to-play"
+          href="https://www.badgersfrommars.com/assets/RegicideRulesA4.pdf"
+          target="_blank" rel="noreferrer"
+        >
+          How to Play
+        </a>
+
+        <div className="navbar-right">
+          {!props.user && (
+            <p onClick={toggleLoginForm} className="login nes-pointer" login>
+              Login
             </p>
-          </div>
-        )}
-      </div>
-    </nav>
+          )}
+          {props.user && (
+            <div className="user-logout">
+              <p>Welcome {props.user.username} |</p>
+              <p className="logout nes-pointer" onClick={logout}>
+                &nbsp;Logout
+              </p>
+            </div>
+          )}
+        </div>
+      </nav>
+      {login && <Login userAuth={userAuth} toggleLoginForm={toggleLoginForm}/>}
+    </>
   );
 }
