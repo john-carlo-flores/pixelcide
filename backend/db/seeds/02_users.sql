3e2bf64b8e1e6841c5be769ddab91c478c0fb760
JC Flores <tothenextcode@gmail.com>
2022-06-13 21:22:36 -0700

    Change user seed password to hashed bcrypt password

--- backend/db/seeds/02_users.sql
+++ backend/db/seeds/02_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (username, name, email, password_digest, avatar_id)
 VALUES 
-('picklerick',      'Rick Sanchez',   'rick.sanchez@gmail.com',  'password', 1),
-('saxophone_lover', 'Lisa Simpson',   'lisa.simpson@gmail.com',  'password', 2),
-('hyrule',          'Link Mario',     'link@yahoo.com',          'password', 3),
-('whipchaingang',   'Simon Belmont',  'simon_bel123@mail.ca',    'password', 4),
-('united_smash',    'Toshinori Yagi', 'all_might@academia.jp',   'password', 5),
-('mamamia',         'Mario Mario',    'mario@mushroomkindom.jp', 'password', 6);
+('picklerick',      'Rick Sanchez',   'rick.sanchez@gmail.com',  '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 1),
+('saxophone_lover', 'Lisa Simpson',   'lisa.simpson@gmail.com',  '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 2),
+('hyrule',          'Link Mario',     'link@yahoo.com',          '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 3),
+('whipchaingang',   'Simon Belmont',  'simon_bel123@mail.ca',    '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 4),
+('united_smash',    'Toshinori Yagi', 'all_might@academia.jp',   '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 5),
+('mamamia',         'Mario Mario',    'mario@mushroomkindom.jp', '$2a$08$ntwCTD/ku8UhcOerXW6jAezp22v9aj331KBPw6Y9uroUpsIAXg64e', 6);
