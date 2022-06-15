bdd04e4b13b3daaabd89bc6483e3d4bc45376375
JC Flores <tothenextcode@gmail.com>
2022-06-14 22:13:41 -0700

    Render each seat in 2x2 pattern with hold on left top most. Pass in props needed in Seat component. Style SeatList.

--- frontend/src/components/GameRoom/SeatList.jsx
+++ frontend/src/components/GameRoom/SeatList.jsx
@@ -1,20 +1,45 @@
 import Seat from './Seat';
-import Empty from './Empty';
+
+import styles from '../../styles/GameRoom/SeatList.module.scss';
 
 const SeatList = (props) => {
   const { user, seats, updateSeatCount, takeSeat } = props;
 
   return (
-    <>
-      <div>
-        <Seat />   
-        <Seat />
+    <div className={styles.main}>
+      <div className={styles.seatRow}>
+        <Seat 
+          user={user}
+          number={1}
+          playerSeated={seats.host}
+          updateSeatCount={updateSeatCount}
+          takeSeat={takeSeat}
+        />
+        <Seat 
+          user={user}
+          number={2}
+          playerSeated={seats.player2}
+          updateSeatCount={updateSeatCount}
+          takeSeat={takeSeat}
+        />
       </div>
-      <div>
-        <Seat />   
-        <Seat />
+      <div className={styles.seatRow}>
+        <Seat 
+          user={user}
+          number={3}
+          playerSeated={seats.player3}
+          updateSeatCount={updateSeatCount}
+          takeSeat={takeSeat}
+        />
+        <Seat 
+          user={user}
+          number={4}
+          playerSeated={seats.player4}
+          updateSeatCount={updateSeatCount}
+          takeSeat={takeSeat}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
