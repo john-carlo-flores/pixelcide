8db1a2ccb3429618585c1310988ab38fd634a969
JC Flores <tothenextcode@gmail.com>
2022-06-15 23:07:33 -0700

    Add websocket listeners on homepage to ping server for lobby creation, cancellation and update. Add error check when room limit is reached and conditional render for lobby creation

--- frontend/src/components/Root/index.jsx
+++ frontend/src/components/Root/index.jsx
@@ -1,11 +1,56 @@
 import Button from "../Button";
 import Navbar from "../Navbar";
-import "../../styles/Homepage.scss";
+import LobbyCreation from "./LobbyCreation";
+import "../../styles/Root/Homepage.scss";
 
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Homepage(props) {
   const { user, userAuth, logout } = props;
+  const [ createLobby, setCreateLobby ] = useState({ create: false });
+  const [ lobby, setLobby ] = useState();
+  
+  const hostGame = () => {
+    user.socket.emit("Create New Lobby");
+  };
+
+  const onCancel = () => {
+    user.socket.emit("Cancel Lobby", lobby);
+    setCreateLobby(false);
+  };
+
+  const assignLobbyTitle = (title) => {
+    console.log(`Assign Lobby with ${title}`);
+    setLobby(prev => {
+      const updatedLobby = {
+        ...prev,
+        title
+      };
+
+      user.socket.emit("Update Lobby", updatedLobby);
+
+      return updatedLobby;
+    });
+  };
+
+  useEffect(() => {
+    // Add listener when new lobby is created if user is logged in
+    user?.socket.on("Get Created Lobby", (createdLobby) => {
+      if (!createdLobby) {
+        return setCreateLobby({
+          error: "Reached maximum amount of rooms alloted. Please join existing games or try again later."
+        });
+      }
+
+      // Allow lobby creation and store lobby values
+      setCreateLobby(prev => {
+        setLobby(createdLobby);
+        return { create: true }
+      });
+      
+    });
+  }, [user]);
 
   return (
     <>
@@ -16,7 +61,7 @@ export default function Homepage(props) {
         <div className="Buttons">
           {props.user && (
             <>
-              <Link to="games/1"><Button error>Host Game</Button></Link>
+              <Button onClick={hostGame} error>Host Game</Button>
               <Link to="games"><Button error>Join Game</Button></Link>
               <Link to="statistics"><Button error>Statistics</Button></Link>
               <Link to="leaderboard"><Button error>Leaderboard</Button></Link>
@@ -25,6 +70,8 @@ export default function Homepage(props) {
           {!props.user && <Link to="signup"><Button error>Sign Up</Button></Link>}
         </div>
       </div>
+      {createLobby.create && <LobbyCreation onCancel={onCancel} assignTitle={assignLobbyTitle} link={lobby.link}/>}
+      {createLobby.error && <p>{createLobby.error}</p>}
     </>
   );
 }
