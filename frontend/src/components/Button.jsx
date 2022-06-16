84c30037cf6e67eb4b0d3b8049ed9cfec4f9bb38
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-16 12:53:13 -0500

    add disabled class

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,13 @@
 import classNames from 'classnames';
 
 export default function Button(props) {
-  let buttonClass = classNames('nes-btn', 
-    { 'is-primary': props.primary }, 
-    { 'is-success': props.success }, 
-    { 'is-warning': props.warning }, 
-    { 'is-error': props.error }
+  let buttonClass = classNames(
+    'nes-btn',
+    { 'is-primary': props.primary },
+    { 'is-success': props.success },
+    { 'is-warning': props.warning },
+    { 'is-error': props.error },
+    { 'is-disabled': props.disabled }
   );
 
   return (
