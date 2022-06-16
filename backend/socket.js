ed319d4d9d3766a17993d4931dec9041ad1c6e68
JC Flores <tothenextcode@gmail.com>
2022-06-15 19:52:39 -0700

    Implement socket handlers to manage lobby creations and user logout socket session

--- backend/socket.js
+++ backend/socket.js
@@ -1,5 +1,6 @@
 const { Server } = require('socket.io');
 const { verify } = require('./helpers/authentication');
+const { createLobby } = require('./helpers/lobby');
 
 module.exports = (sessionMiddleware, httpServer) => {
   const io = new Server(httpServer);
@@ -9,7 +10,19 @@ module.exports = (sessionMiddleware, httpServer) => {
   
   io.use(wrap(sessionMiddleware));
 
-  io.on("connection", verify, (socket) => {
-    console.log("User connected!");
+  io.on("connection", (socket, title) => {
+    console.log(`User ${socket.id} connected!`);
+
+    socket.on("Create New Lobby", (title) => {
+      console.log("Create New Lobby: ", title); 
+      const lobby = createLobby(socket.id, title);
+
+      socket.emit("Get New Lobby", lobby);
+    });
+
+    socket.on("logout", (args) => {
+      socket.disconnect();
+    });
+
   });
 };
