2e6e6a67d51cb355d2f3c463fcb173bee1db521a
JC Flores <tothenextcode@gmail.com>
2022-06-17 13:45:19 -0700

    Add sockets and request current list of lobbies on render

--- frontend/src/components/Games/index.jsx
+++ frontend/src/components/Games/index.jsx
@@ -1,18 +1,33 @@
 import LobbyList from "./LobbyList";
 import Navbar from "../Navbar";
+import FilterLobby from "./FilterLobby";
 
+import { SocketContext } from "../../context/socket";
+import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/Games/Games.module.scss";
 
 const Games = (props) => {
   const { userAuth, user, logout } = props;
+  const [ lobbies, setLobbies ] = useState({});
+  
+  const socket = useContext(SocketContext);
 
-  return(
+  useEffect(() => {
+    socket.emit("Request Lobbies");
+
+    socket.on("Get Lobbies", (lobbies) => {
+      setLobbies(lobbies);
+    });
+  }, []);
+
+  return (
     <>
       <div className={styles.Homepage}></div>
       <Navbar userAuth={userAuth} user={user} logout={logout} />
-      <LobbyList />
+      <FilterLobby lobbies={lobbies} setLobbies={lobbies} />
+      <LobbyList lobbies={lobbies}/>
       <Link to="/">Back to home page</Link>
     </>
   );
