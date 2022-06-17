fe70c698e94a201f9846ade6aa0a7ad68ad0bcac
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-17 00:23:37 -0500

    add playerField animation

--- frontend/src/components/Game/Player.jsx
+++ frontend/src/components/Game/Player.jsx
@@ -1,5 +1,6 @@
 import Card from './Card';
 import '../../styles/Game/Player.scss';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Player = (props) => {
   const { playerCards, playerName, avatar, setPlayerField, playerField, setPlayerCards } = props;
@@ -9,27 +10,33 @@ const Player = (props) => {
     const newPlayerCards = [...playerCards].filter((item) => item.id !== card.id);
 
     setPlayerCards(newPlayerCards);
-    setPlayerField((prev) => [...prev, card]);
+
+    setTimeout(() => {
+      setPlayerField((prev) => [...prev, card]);
+    }, 200);
   };
 
   return (
     <div className="Player">
-      <div className="player-field">
+      <motion.div initial={false} className="player-field">
         {playerField.map((card) => (
-          <div key={card.id} className="player-field-card">
+          <motion.div layout key={card.id} className="player-field-card">
             <Card image={card.image_front} />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
 
-      <div className="cards-container">
-        {playerCards.map((card) => (
-          <div key={card.id} onClick={() => handleClick(card)} className="player-card">
-            <Card image={card.image_front} />
-          </div>
-        ))}
+      <motion.div className="cards-container">
+        <AnimatePresence>
+          {playerCards.map((card) => (
+            <motion.div exit={{ y: -250, x: 0 }} transition={{ ease: 'easeIn' }} key={card.id} onClick={() => handleClick(card)} className="player-card">
+              <Card image={card.image_front} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+
         {playerCards.length === 0 && <div className="empty"></div>}
-      </div>
+      </motion.div>
 
       <div className="player-info">
         <img src={`https://raw.githubusercontent.com/tothenextcode/pixelcide/feature/frontend/Game-UI/frontend/src/assets/avatars/${avatar}.png`} alt="user avatar" />
