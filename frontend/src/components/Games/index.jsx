192ee71dcb2c9f1f9506a8ba3d08f06ec595a946
JC Flores <tothenextcode@gmail.com>
2022-06-17 22:46:01 -0700

    Add comments to Games component for readability

--- frontend/src/components/Games/index.jsx
+++ frontend/src/components/Games/index.jsx
@@ -6,21 +6,25 @@ import { SocketContext } from "../../context/socket";
 import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import { IoArrowBackCircle } from "react-icons/io5"
+import { IoArrowBackCircle } from "react-icons/io5";
 import styles from "../../styles/Games/Games.module.scss";
 
 const Games = (props) => {
   const { userAuth, user, logout } = props;
-  const [ lobbies, setLobbies ] = useState();
-  
+  const [lobbies, setLobbies] = useState();
+
   const socket = useContext(SocketContext);
 
   useEffect(() => {
+    // Request list of lobbies to render
     socket.emit("Request Lobbies");
 
+    // Render list of lobbies
     socket.on("Get Lobbies", (lobbies) => {
       setLobbies(lobbies);
     });
+
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -29,9 +33,11 @@ const Games = (props) => {
       <Navbar userAuth={userAuth} user={user} logout={logout} />
       <div className={styles.container}>
         <FilterLobby lobbies={lobbies} setLobbies={lobbies} />
-        {lobbies && <LobbyList lobbies={lobbies}/>}
+        {lobbies && <LobbyList lobbies={lobbies} />}
       </div>
-      <Link to="/" className={styles.back}><IoArrowBackCircle size={80}/></Link>
+      <Link to="/" className={styles.back}>
+        <IoArrowBackCircle size={80} />
+      </Link>
     </>
   );
 };
