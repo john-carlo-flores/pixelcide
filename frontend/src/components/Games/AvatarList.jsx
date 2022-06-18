a361de860c010a42ea89e21ecd84a12d32b436ca
JC Flores <tothenextcode@gmail.com>
2022-06-18 01:55:06 -0700

    Space out each avatar in list and provide full url to each Avatar

--- frontend/src/components/Games/AvatarList.jsx
+++ frontend/src/components/Games/AvatarList.jsx
@@ -4,13 +4,13 @@ import styles from "../../styles/Games/AvatarList.module.scss";
 
 const AvatarList = (props) => {
   const { avatars } = props;
-  const avatarList = [];
-  
-  return (
-    <div className={styles.container}>
-      {avatarList}
-    </div>
-  );
+  const baseURL =
+    "https://raw.githubusercontent.com/tothenextcode/pixelcide/feature/frontend/Game-UI/frontend/src/assets/avatars/";
+  const avatarList = avatars.map((avatar) => {
+    return <Avatar key={avatar} url={`${baseURL}${avatar}.png`} />;
+  });
+
+  return <div className={styles.container}>{avatarList}</div>;
 };
 
 export default AvatarList;
