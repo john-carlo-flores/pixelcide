be9140683d7e16d81ebe5bfafd6025d73517d054
JC Flores <tothenextcode@gmail.com>
2022-06-18 01:58:30 -0700

    Style Lobby and add logic to calculate seat information. Provide avatar list to AvatarList

--- frontend/src/components/Games/Lobby.jsx
+++ frontend/src/components/Games/Lobby.jsx
@@ -1,36 +1,63 @@
 import Button from "../Button";
 import AvatarList from "./AvatarList";
 
+import { Link } from "react-router-dom";
+
 import styles from "../../styles/Games/Lobby.module.scss";
 
 const Lobby = (props) => {
   const { lobby, seats } = props;
-  const seatCount = "1/4";
-  const full = false;
 
-  const avatars = [];
+  // Get number of seats and maxSeats from lobby.game.players
+  const { seatCount, maxSeats } = seats.reduce(
+    (total, seat) => {
+      total.maxSeats += seat !== "none" ? 1 : 0;
+      total.seatCount += seat?.id ? 1 : 0;
+      return total;
+    },
+    { maxSeats: 0, seatCount: 0 }
+  );
+
+  // Prepare seat information to display
+  const seatStatus = `${seatCount}/${maxSeats}`;
+  const full = seatCount === maxSeats;
+
+  const mode = lobby.mode;
+
+  // list valid avatar ids in filled seats
+  const avatars = seats.map((seat) => seat.avatar_id).filter((id) => id);
 
   return (
-    <div>
+    <div className={`${styles.container} nes-container is-rounded`}>
       <div className={styles.header}>
         <div className={styles.titles}>
           <h2>LOBBY: {lobby.title}</h2>
           <h2>HOST: {lobby.host}</h2>
         </div>
-        <h2 className={styles.seats}>{seatCount}</h2>
+        <h2 className={styles.seats}>{seatStatus}</h2>
       </div>
       <div className={styles.footer}>
         {full ? (
-          <Button error></Button>
+          <div className="nes-badge">
+            <span className="is-error">Full</span>
+          </div>
         ) : (
           <span className={styles.empty}></span>
         )}
-        <AvatarList avatars={avatars} />
-        {full ? (
-          <Button error>Spectate</Button>
-        ) : (
-          <Button success>Join Game</Button>
-        )}
+        <div className={styles.rightFooter}>
+          <AvatarList avatars={avatars} />
+          {full && mode === "Game" && (
+            <Link to={`/games/${lobby.link}`}>
+              <Button success>Spectate</Button>
+            </Link>
+          )}
+          {!full && mode !== "Game" && (
+            <Link to={`/games/${lobby.link}`}>
+              <Button success>Join Game</Button>
+            </Link>
+          )}
+          {full && mode !== "Game" && <span className={styles.empty}></span>}
+        </div>
       </div>
     </div>
   );
