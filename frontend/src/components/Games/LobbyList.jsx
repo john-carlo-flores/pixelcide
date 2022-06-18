1310c4c353f25deed843a103750cefa85f324812
JC Flores <tothenextcode@gmail.com>
2022-06-17 22:13:26 -0700

    Add key to diferrentiate between Lobby renders

--- frontend/src/components/Games/LobbyList.jsx
+++ frontend/src/components/Games/LobbyList.jsx
@@ -2,23 +2,14 @@ import Lobby from "./Lobby";
 
 import styles from "../../styles/Games/LobbyList.module.scss";
 
-const LobbyList = (props) => {  
+const LobbyList = (props) => {
   const { lobbies } = props;
-  
-  const lobbyList = lobbies?.map(lobby => {
-    return (
-      <Lobby
-        lobby={lobby}
-        seats={lobby.game.players}
-      />
-    )
+
+  const lobbyList = lobbies?.map((lobby) => {
+    return <Lobby key={lobby.link} lobby={lobby} seats={lobby.game.players} />;
   });
 
-  return (
-    <div className={styles.container}>
-      {lobbyList}
-    </div>
-  );
+  return <div className={styles.container}>{lobbyList}</div>;
 };
 
 export default LobbyList;
