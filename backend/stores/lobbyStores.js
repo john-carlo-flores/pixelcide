0cf77947165c46bb958b0d21c91996eb67be1e41
JC Flores <tothenextcode@gmail.com>
2022-06-18 22:00:19 -0700

    Prettier formatting

--- backend/stores/lobbyStores.js
+++ backend/stores/lobbyStores.js
@@ -1,4 +1,4 @@
-const { generateUniqueLink } = require('../helpers/authentication');
+const { generateUniqueLink } = require("../helpers/authentication");
 
 const MAX_NUM_ROOMS = 20;
 const LINK_LENGTH = 8;
@@ -6,7 +6,7 @@ const LINK_LENGTH = 8;
 class LobbyStore {
   constructor() {
     this.lobbies = new Map();
-  };
+  }
 
   createLobby(host) {
     if (this.lobbies.length === MAX_NUM_ROOMS) {
@@ -19,47 +19,47 @@ class LobbyStore {
     // Create lobby object
     const newLobby = {
       host: host.username,
-      title: '',
+      title: "",
       link,
       game: {
-        players: ['none', 'none', 'none', 'none']
-      }
+        players: ["none", "none", "none", "none"],
+      },
     };
 
     // store new lobby and return
     this.addUserToLobby(newLobby, 0, host);
     return this.lobbies.set(link, newLobby).get(link);
-  };
+  }
 
   getLobby(link) {
     return this.lobbies.get(link);
-  };
+  }
 
   cancelLobby(lobby) {
     this.lobbies.delete(lobby.link);
-  };
+  }
 
   listLobbies() {
     console.log([...this.lobbies.values()]);
     return [...this.lobbies.values()];
-  };
+  }
 
   updateLobby(lobby) {
     const prevLobby = this.lobbies.get(lobby.link);
 
     const updatedLobby = {
       ...prevLobby,
-      ...lobby
+      ...lobby,
     };
 
     // Store updated lobby and return
     this.lobbies.set(lobby.link, updatedLobby);
     return updatedLobby;
-  };
+  }
 
   addUserToLobby(lobby, seat, user) {
     lobby.game.players[seat] = user;
-  };
+  }
 }
 
 module.exports = { LobbyStore };
