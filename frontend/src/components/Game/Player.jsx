52c4f9f9ebc993970bcbdf24186b6d7e1c4f884a
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-19 01:59:11 -0500

    fix card animation in playerhand

--- frontend/src/components/Game/Player.jsx
+++ frontend/src/components/Game/Player.jsx
@@ -1,8 +1,8 @@
-import useSound from 'use-sound';
-import Card from './Card';
-import '../../styles/Game/Player.scss';
-import { motion, AnimatePresence } from 'framer-motion';
-import cardFlipSound from '../../assets/sounds/card-flip-.mp3';
+import useSound from "use-sound";
+import Card from "./Card";
+import "../../styles/Game/Player.scss";
+import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
+import cardFlipSound from "../../assets/sounds/card-flip-.mp3";
 
 const Player = (props) => {
   const { playerCards, playerName, avatar, setPlayerField, playerField, setPlayerCards, status, currentBoss } = props;
@@ -15,39 +15,39 @@ const Player = (props) => {
       return true;
     }
     //if any one card is in player field then only an Ace can be played (except Jester)
-    if (playerField.length === 1 && playerField[0].tag === 'A' && theCard.tag !== 'Jester') {
+    if (playerField.length === 1 && playerField[0].tag === "A" && theCard.tag !== "Jester") {
       return true;
     }
     //if an ace in in player field any card can be played (except Jester)
-    if (playerField.length === 1 && playerField[0].tag !== 'Jester' && theCard.tag === 'A') {
+    if (playerField.length === 1 && playerField[0].tag !== "Jester" && theCard.tag === "A") {
       return true;
     }
     //2 card combos (2-2, 3-3, 4-4, 5-5)
     if (
       playerField.length === 1 &&
-      ((playerField[0].tag === '2' && theCard.tag === '2') ||
-        (playerField[0].tag === '3' && theCard.tag === '3') ||
-        (playerField[0].tag === '4' && theCard.tag === '4') ||
-        (playerField[0].tag === '5' && theCard.tag === '5'))
+      ((playerField[0].tag === "2" && theCard.tag === "2") ||
+        (playerField[0].tag === "3" && theCard.tag === "3") ||
+        (playerField[0].tag === "4" && theCard.tag === "4") ||
+        (playerField[0].tag === "5" && theCard.tag === "5"))
     ) {
       return true;
     }
     //3 card combos (2-2-2, 3-3-3)
     if (
       playerField.length === 2 &&
-      ((playerField[0].tag === '2' && playerField[1].tag === '2' && theCard.tag === '2') || (playerField[0].tag === '3' && playerField[1].tag === '3' && theCard.tag === '3'))
+      ((playerField[0].tag === "2" && playerField[1].tag === "2" && theCard.tag === "2") || (playerField[0].tag === "3" && playerField[1].tag === "3" && theCard.tag === "3"))
     ) {
       return true;
     }
     //4 card combo (2-2-2-2)
-    if (playerField.length === 3 && playerField[0].tag === '2' && playerField[1].tag === '2' && playerField[2].tag === '2' && theCard.tag === '2') {
+    if (playerField.length === 3 && playerField[0].tag === "2" && playerField[1].tag === "2" && playerField[2].tag === "2" && theCard.tag === "2") {
       return true;
     }
   };
 
   //click handler to move cards to player field
   const moveCardToPlayerField = (card) => {
-    if (status === 'boss_attack' || (status === 'player_attack' && playable(card))) {
+    if (status === "boss_attack" || (status === "player_attack" && playable(card))) {
       const newPlayerCards = [...playerCards].filter((item) => item.id !== card.id);
       playOn();
 
@@ -69,33 +69,38 @@ const Player = (props) => {
 
   return (
     <div className="Player">
-      <motion.div className="player-field">
-        {playerField.map((card) => (
-          <motion.div layout transition={{ ease: 'easeIn', duration: 0.4, opacity: 0 }} onClick={() => moveCardToPlayerHand(card)} key={card.id} className="player-field-card">
-            <Card image={card.image_front} warning={card.suit === currentBoss.suit && status !== 'boss_attack' ? 'warning' : ''} />
-          </motion.div>
-        ))}
-      </motion.div>
-
-      <motion.div className="cards-container">
-        <AnimatePresence>
-          {playerCards.map((card) => (
-            <motion.div
-              exit={{ y: -250, x: 0 }}
-              transition={{ ease: 'easeIn', duration: 0.4 }}
-              whileHover={{
-                y: playable(card) && -20,
-                transition: { duration: 0 },
-                x: playable(card) && -70,
-              }}
-              key={card.id}
-              onClick={() => moveCardToPlayerField(card)}
-              className={playable(card) ? 'player-card highlight' : 'player-card dull'}
-            >
-              <Card image={card.image_front} />
+      <LayoutGroup>
+        <motion.div className="player-field">
+          {playerField.map((card) => (
+            <motion.div layout transition={{ ease: "easeIn", duration: 0.4, opacity: 0 }} onClick={() => moveCardToPlayerHand(card)} key={card.id} className="player-field-card">
+              <Card image={card.image_front} warning={card.suit === currentBoss.suit && status !== "boss_attack" ? "warning" : ""} />
             </motion.div>
           ))}
-        </AnimatePresence>
+        </motion.div>
+      </LayoutGroup>
+
+      <motion.div className="cards-container">
+        <LayoutGroup>
+          <AnimatePresence>
+            {playerCards.map((card) => (
+              <motion.div
+                layout
+                exit={{ y: -250, x: 0 }}
+                transition={{ ease: "easeIn", duration: 0.4 }}
+                whileHover={{
+                  y: playable(card) && -20,
+                  transition: { duration: 0 },
+                  x: playable(card) && -70,
+                }}
+                key={card.id}
+                onClick={() => moveCardToPlayerField(card)}
+                className={playable(card) ? "player-card highlight" : "player-card dull"}
+              >
+                <Card image={card.image_front} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </LayoutGroup>
       </motion.div>
 
       <div className="player-info">
