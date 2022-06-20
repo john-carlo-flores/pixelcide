736c98286f631de7f96cabefdcc20eecc2a60a80
Muhammed Tariq <momotrq94@gmail.com>
2022-06-20 15:13:23 -0400

    create leaderboard route

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const session = require('express-session');
-const http = require('http');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const session = require("express-session");
+const http = require("http");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const db = require('./configs/db.config');
+const db = require("./configs/db.config");
 
-const socket = require('./socket');
+const socket = require("./socket");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const gamesRouter = require('./routes/games');
-const cardsRouter = require('./routes/cards');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const cardsRouter = require("./routes/cards");
+const leaderboardRouter = require("./routes/leaderboard");
 
 const app = express();
 
@@ -21,24 +22,25 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionMiddleware = session({
   secret: process.env.SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
 });
 
 app.use(sessionMiddleware);
 socket(sessionMiddleware, server);
 
-app.use('/', indexRouter(db));
-app.use('/users', usersRouter(db));
-app.use('/games', gamesRouter(db));
-app.use('/cards', cardsRouter(db));
+app.use("/", indexRouter(db));
+app.use("/users", usersRouter(db));
+app.use("/games", gamesRouter(db));
+app.use("/cards", cardsRouter(db));
+app.use("/leaderboard", leaderboardRouter(db));
 
 module.exports = { app, server };
