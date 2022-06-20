4f1e6151e168538e9992d592d74884468c1be7e2
JC Flores <tothenextcode@gmail.com>
2022-06-20 15:12:25 -0700

    Add useEffects to track game conditions and implement moveCardTo

--- frontend/src/hooks/useGame.js
+++ frontend/src/hooks/useGame.js
@@ -16,7 +16,12 @@ import {
   clearPlayfield,
 } from "../helpers/player-helpers";
 
-import { updateCycle, resetCycle } from "../helpers/turn-helpers";
+import {
+  updateCycle,
+  resetCycle,
+  playerDead,
+  playerDefense,
+} from "../helpers/turn-helpers";
 
 import { updateBossCondition } from "../helpers/boss-helpers";
 
@@ -30,100 +35,182 @@ const useGame = () => {
   const [cycle, setCycle] = useState([]);
   const [players, setPlayers] = useState([]);
   const [status, setStatus] = useState("setup");
-  const [validateButton, setValidateButton] = useState([]);
-  let maxHand = 0;
+  const [validate, setValidate] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState({});
+  const [maxHand, setMaxHand] = useState(0);
+  const [started, setStarted] = useState(false);
+
+  useEffect(() => {
+    // On player turn start, turn if then have cards to play
+    if (status === "player_turn") {
+      setTimeout(() => {
+        if (currentPlayer.hand.length <= 0) {
+          setStatus("game_over_lose");
+        } else {
+          setStatus("player_attack");
+        }
+      }, 2000);
+    }
+
+    // On boss attack, check if current player has enough cards to defend
+    if (status === "boss_attack") {
+      if (playerDead(currentPlayer, boss.damage)) {
+        setStatus("game_over_lose");
+      }
+    }
+
+    // Reset joker on game end
+    if (status === "game_over_lose" || status === "game_over_win") {
+      setBoss((prev) => {
+        const bossCopy = _.cloneDeep(prev);
+        bossCopy.stats.powerEnabled = false;
+
+        return bossCopy;
+      });
+    }
+  }, [status]);
+
+  useEffect(() => {
+    // Validate player discard amount is enough to defend
+    if (status === "boss_attack") {
+      const { enoughDefense, damageRemaining } = playerDefense(
+        currentPlayer,
+        boss.damage
+      );
+
+      setValidate((prev) => {
+        return {
+          ...prev,
+          discardButton: enoughDefense,
+          discardValue: damageRemaining,
+        };
+      });
+    }
+
+    // validate that player played a card to enable button
+    if (status === "player_attack") {
+      setValidate((prev) => {
+        return {
+          ...prev,
+          attackButton: currentPlayer.field.length > 0,
+        };
+      });
+    }
+  }, [status, currentPlayer]);
 
   const setGame = (game) => {
+    // If game is already started, setGame state on refresh
     setDecks(_.cloneDeep(game.decks));
     setBoss(_.cloneDeep(game.boss));
     setCycle(_.cloneDeep(game.cycle));
-    setPlayers(_.cloneDeep(game.players));
+
+    // Use a copy so currentPlayer still references players array
+    const playersCopy = _.cloneDeep(game.players);
+    setPlayers(playersCopy);
+    setCurrentPlayer(playersCopy[game.cycle.current[0]]);
+
     setStatus(_.cloneDeep(game.status));
-    setValidateButton(_.cloneDeep(game.validateButton));
+    setValidate(_.cloneDeep(game.validateButton));
+    setMaxHand(game.maxHand);
+    setStarted(game.started);
   };
 
   const setup = (playerList) => {
     // Get max hand size based on number of players
-    maxHand = calcMaxHand(playerList.length);
+    const _maxHand = calcMaxHand(playerList.length);
+    setMaxHand(_maxHand);
+
+    console.log("Setting up!");
 
     // Request for list of all cards from api server
-    axios.get("cards").then((response) => {
-      const cards = response.data;
+    axios
+      .get("/cards")
+      .then((response) => {
+        const cards = response.data;
 
-      setDecks(() => {
-        const castleDeck = makeCastle(cards);
-        const tavernDeck = makeTavern(cards);
+        setDecks(() => {
+          const castleDeck = makeCastle(cards);
+          const tavernDeck = makeTavern(cards);
 
-        // Assign boss card and stats
-        setBoss(() => {
-          const lastCastleCard = castleDeck.at(-1);
+          // Assign boss card and stats
+          setBoss(() => {
+            const lastCastleCard = castleDeck.at(-1);
 
-          const boss = {
-            current: lastCastleCard,
-            stats: {
-              damage: lastCastleCard.damage,
-              health: lastCastleCard.health,
-              suit: lastCastleCard.suit,
-              powerEnabled: true,
-            },
-          };
-
-          return boss;
-        });
-
-        // Assign starting hand for each player
-        setPlayers(() => {
-          const setupPlayers = playerList.map((player) => {
-            return {
-              ...player,
-              hand: makeHand(tavernDeck, maxHand),
-              field: [],
-              discard: [],
-              played: [],
+            const boss = {
+              current: lastCastleCard,
+              stats: {
+                damage: lastCastleCard.damage,
+                health: lastCastleCard.health,
+                suit: lastCastleCard.suit,
+                powerEnabled: true,
+              },
             };
+
+            return boss;
           });
 
-          //
-          return setupPlayers;
-        });
+          // Assign starting hand for each player
+          setPlayers(() => {
+            const setupPlayers = playerList.map((player) => {
+              return {
+                ...player,
+                hand: makeHand(tavernDeck, _maxHand),
+                field: [],
+                discard: [],
+                played: [],
+              };
+            });
 
-        /* Set Cycle (Player turn order)
+            //Assign currentPlayer as a reference
+            setCurrentPlayer(setupPlayers[0]);
+
+            return setupPlayers;
+          });
+
+          /* Set Cycle (Player turn order)
             - original does not change
               > Only changes if current player kills boss
             - current changes every turn. First element is shifted out
               > To reset current, set to original
         */
-        setCycle({
-          original: [...Array(playerList.length).keys()],
-          current: [...Array(playerList.length).keys()],
-        });
+          setCycle({
+            original: [...Array(playerList.length).keys()],
+            current: [...Array(playerList.length).keys()],
+          });
 
-        /* Set ValidateButton (Handles enabling of Discard and Attack button) 
+          /* Set ValidateButton (Handles enabling of Discard and Attack button) 
               - discard -> true if enough discard amount
               - attack -> true if valid attack
               - discardVal -> remaining amount required to discard
         */
-        setValidateButton({
-          discard: false,
-          attack: false,
-          discardVal: 0,
+          setValidate({
+            discardButton: false,
+            attackButton: false,
+            discardValue: 0,
+          });
+
+          // Change status to player_turn once setup is complete
+          setStatus("player_turn");
+
+          // Setup starting decks
+          return {
+            discard: [],
+            tavern: tavernDeck,
+            castle: castleDeck,
+          };
         });
 
-        // Change status to player_turn once setup is complete
-        setStatus("player_turn");
-
-        // Setup starting decks
-        return {
-          discard: [],
-          tavern: tavernDeck,
-          castle: castleDeck,
-        };
+        setTimeout(() => {
+          setStarted(true);
+        }, 400);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
   };
 
   const handleYield = () => {
-    setStatus("boss_turn");
+    setStatus("boss_attack");
   };
 
   const handlePlayerAttack = () => {
@@ -133,7 +220,7 @@ const useGame = () => {
     const bossCopy = _.cloneDeep(boss);
     const playersCopy = _.cloneDeep(players);
     const cycleCopy = _.cloneDeep(cycle);
-    const currentPlayer = playersCopy[cycle.original[0]];
+    const currentPlayerIndex = cycleCopy.current[0];
 
     // STEP 1: Play Cards / Yield
     // Get Suit Power values for activation and total damage
@@ -161,7 +248,7 @@ const useGame = () => {
       activateDiamondPower(
         diamondPower,
         playersCopy,
-        cycleCopy.current[0],
+        currentPlayerIndex,
         cycleCopy.original,
         tavernDeck,
         maxHand
@@ -193,17 +280,16 @@ const useGame = () => {
       bossCopy,
       castleDeck,
       discardDeck,
-      tavernDeck,
-      setStatus
+      tavernDeck
     );
 
     // commit cards on field to played
-    commitPlayfield(playersCopy, currentPlayer);
+    commitPlayfield(playersCopy, currentPlayerIndex);
 
     // STEP 4: Suffer damage from boss
     // boss still alive, switch status to boss attack
     if (!bossCondition.defeated) {
-      setStatus("boss_turn");
+      setStatus("boss_attack");
     }
 
     // If dead, keep same player but reset cycle
@@ -233,19 +319,24 @@ const useGame = () => {
   const handlePlayerDiscard = () => {
     const discardDeck = _.cloneDeep(decks.discard);
     const playersCopy = _.cloneDeep(players);
-    const currentPlayer = playersCopy[cycle.current[0]];
     const cycleCopy = _.cloneDeep(cycle);
+    const currentPlayerIndex = cycleCopy.current[0];
 
     // Move discarded cards from playerfield to discard
     setDecks([...discardDeck.discard, ...currentPlayer.discard]);
-    currentPlayer.discard = []; // clear player discard
+    playersCopy[currentPlayerIndex].discard = []; // clear player discard
 
-    // Go to next player in cycle
+    // Set next player in cycle
     const cycleReset = updateCycle(cycleCopy);
     if (cycleReset) resetCycle(cycleCopy);
 
-    // Save player changes
+    // Save changes
     setPlayers(playersCopy);
+    setCycle(cycleCopy);
+    setCurrentPlayer(playersCopy[cycleCopy.current[0]]);
+
+    // Go to next player
+    setStatus("player_turn");
   };
 
   const handleGameOver = (condition) => {
@@ -280,7 +371,95 @@ const useGame = () => {
     }
   };
 
-  return { setup, setGame, handleCommands, status, boss };
+  const moveCardTo = (card, area, playable = true, playCardSound = null) => {
+    switch (area) {
+      case "Hand":
+        // Copy players to modify and track current player
+        const playersCopy = _.cloneDeep(players);
+        const currentPlayer = playersCopy[cycle.current[0]];
+
+        // Remove from discard if boss attack
+        if (status === "boss_attack") {
+          currentPlayer.discard = currentPlayer.discard.filter(
+            (item) => item.id !== card.id
+          );
+        }
+
+        // Remove from field if player attack
+        if (status === "player_attack") {
+          currentPlayer.field = currentPlayer.field.filter(
+            (item) => item.id !== card.id
+          );
+        }
+
+        // Add card to player hand and save
+        currentPlayer.hand = [...currentPlayer.hand, card];
+        setPlayers(playersCopy);
+        break;
+      case "Discard":
+        if (status === "boss_attack") {
+          // Copy players to modify and track current player
+          const playersCopy = _.cloneDeep(players);
+          const currentPlayer = playersCopy[cycle.current[0]];
+
+          // Remove card to move from player hand
+          currentPlayer.hand = currentPlayer.hand.filter(
+            (item) => item.id !== card.id
+          );
+
+          playCardSound();
+
+          // Save player hand
+          setPlayers(playersCopy);
+
+          setTimeout(() => {
+            // Add card to player discard and save
+            currentPlayer.discard = [...currentPlayer.discard, card];
+            setPlayers(playersCopy);
+          }, 400);
+        }
+        break;
+      case "Field":
+        if (status === "player_attack" && playable) {
+          // Copy players to modify and track current player
+          const playersCopy = _.cloneDeep(players);
+          const currentPlayer = playersCopy[cycle.current[0]];
+
+          // Remove card to move from player hand
+          currentPlayer.hand = currentPlayer.hand.filter(
+            (item) => item.id !== card.id
+          );
+
+          playCardSound();
+
+          // Save player hand
+          setPlayers(playersCopy);
+
+          setTimeout(() => {
+            // Add card to player field and save
+            currentPlayer.field = [...currentPlayer.field, card];
+            setPlayers(playersCopy);
+          }, 400);
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return {
+    setup,
+    setGame,
+    started,
+    handleCommands,
+    moveCardTo,
+    players,
+    currentPlayer,
+    boss,
+    status,
+    validate,
+    decks,
+  };
 };
 
 export default useGame;
