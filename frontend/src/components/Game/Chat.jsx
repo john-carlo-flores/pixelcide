c6cc26c2b52f940411d96222ee782bf00f48ff8b
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-19 19:13:12 -0500

    add useOnClickOutside hook and animations to new chat bubbles

--- frontend/src/components/Game/Chat.jsx
+++ frontend/src/components/Game/Chat.jsx
@@ -1,16 +1,19 @@
 import chatIcon from "../../assets/icons/chat.svg";
 import "../../styles/Game/Chat.scss";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState, useRef } from "react";
+import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 import Button from "../Button";
 import crappyAvatar from "../../assets/avatars/2.png";
 import moreCrappyDrawing from "../../assets/avatars/7.png";
 
 const Chat = () => {
+  //to check click outside the chat component
+  const ref = useRef();
+
   const [toggleChat, setToggleChat] = useState(false);
   const [chatInput, setChatInput] = useState("");
-
   const [userChat, setUserChat] = useState([]);
 
   const handleToggle = () => {
@@ -27,27 +30,28 @@ const Chat = () => {
     setChatInput(e.target.value);
   };
 
+  useOnClickOutside(ref, () => setToggleChat(false));
   return (
-    <div className="Chat ">
+    <div className="Chat" ref={ref}>
       <AnimatePresence>
         {toggleChat && (
           <motion.div initial={{ y: 50 }} animate={{ y: -20 }} exit={{ y: 100, opacity: 0 }} className="outer-div">
             <div className="container">
-              {userChat.map((chat) => (
-                <div className="input ">
+              {userChat.map((chat, i) => (
+                <motion.div initial={{ y: 20 }} animate={{ y: 0 }} key={i} className="input ">
                   <div className="avatar">
                     <img src={crappyAvatar} alt="" />
                   </div>
                   <div className="content ">{chat}</div>
-                </div>
+                </motion.div>
               ))}
 
-              <div className="output ">
+              <motion.div layout initial={{ y: 20 }} animate={{ y: 0 }} className="output ">
                 <div className="content">What's up</div>
                 <div className="avatar">
                   <img src={moreCrappyDrawing} alt="" />
                 </div>
-              </div>
+              </motion.div>
             </div>
             <form onSubmit={(e) => handleSubmit(e)} action="POST">
               <input type="text" onChange={(e) => handleChange(e)} value={chatInput} />
@@ -57,7 +61,7 @@ const Chat = () => {
         )}
       </AnimatePresence>
 
-      <div onClick={handleToggle} className="chat-icon nes-pointer">
+      <div onClick={handleToggle} className={toggleChat ? "chat-icon nes-pointer glow" : "chat-icon nes-pointer"}>
         <img src={chatIcon} alt="chat-icon" />
       </div>
     </div>
