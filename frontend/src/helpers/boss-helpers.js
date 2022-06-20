5383a58f30a3c2d706f27ff020cf2eb74ce0cea2
JC Flores <tothenextcode@gmail.com>
2022-06-20 10:25:36 -0700

    Update boss condition check to set stats of new boss

--- frontend/src/helpers/boss-helpers.js
+++ frontend/src/helpers/boss-helpers.js
@@ -1,29 +1,33 @@
-export function updateBossCondition(boss, castleDeck, discardDeck, tavernDeck, cycle) {
-  let bossDefeated = false;
-  let gameOver = false;
-  
+export function updateBossCondition(boss, castleDeck, discardDeck, tavernDeck) {
+  let bossCondition = {};
+
   // Check if overkill, put boss to discard
   if (boss.stats.health < 0) {
     tavernDeck.push(castleDeck.pop());
-    bossDefeated = true;
+    bossCondition.defeated = true;
   }
 
   // Check if exact kill, put boss to top of tavern
   if (boss.stats.health === 0) {
     discardDeck.push(castleDeck.pop());
-    bossDefeated = true;
+    bossCondition.defeated = true;
   }
 
   // if no more bosses, change status to game over win
   if (castleDeck.length === 0) {
-    setStatus("game_over_win")
-    gameOver = true;
+    bossCondition.gameOver = true;
   }
 
-  // if boss defeated, go to next boss
-  if (bossDefeated && !gameOver) {
+  // if boss defeated, go to next boss and reset Jester
+  if (bossCondition.defeated && !bossCondition.gameOver) {
     boss.current = castleDeck.at(-1);
+    boss.stats = {
+      damage: boss.current.damage,
+      health: boss.current.health,
+      suit: boss.current.suit,
+      powerEnabled: false,
+    };
   }
 
-  return bossDefeated;
+  return bossCondition;
 }
