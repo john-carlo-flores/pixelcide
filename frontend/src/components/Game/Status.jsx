aaef404fd1d4ce5655dddbd28ed87a0615f69107
JC Flores <tothenextcode@gmail.com>
2022-06-20 15:11:14 -0700

    Use new clickHandler 'handleCommands' and validate object structure

--- frontend/src/components/Game/Status.jsx
+++ frontend/src/components/Game/Status.jsx
@@ -1,26 +1,42 @@
-import '../../styles/Game/Status.scss';
-import Button from '../Button';
-import { Link } from 'react-router-dom';
+import "../../styles/Game/Status.scss";
+import Button from "../Button";
+import { Link } from "react-router-dom";
 
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 
 const Status = (props) => {
-  const { status, handlePlayerAttack, handleBossAttack, validateDiscard, validateAttack, discardVal } = props;
+  const { status, handleCommands, validate } = props;
 
   return (
     <>
       <AnimatePresence>
-        {status === 'player_turn' && (
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: 'easeOut', duration: 0 }} className="player-turn">
+        {status === "player_turn" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ ease: "easeOut", duration: 0 }}
+            className="player-turn"
+          >
             -- YOUR TURN --
           </motion.div>
         )}
       </AnimatePresence>
 
       <AnimatePresence>
-        {status === 'player_attack' && (
-          <motion.div layout initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: -10 }} exit={{ opacity: 0, y: -60 }} transition={{ ease: 'easeInOut', duration: 0.1 }}>
-            <Button onClick={handlePlayerAttack} error disabled={!validateAttack}>
+        {status === "player_attack" && (
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: -10 }}
+            exit={{ opacity: 0, y: -60 }}
+            transition={{ ease: "easeInOut", duration: 0.1 }}
+          >
+            <Button
+              onClick={() => handleCommands("Attack")}
+              error
+              disabled={!validate.attackButton}
+            >
               ATTACK
             </Button>
           </motion.div>
@@ -28,17 +44,30 @@ const Status = (props) => {
       </AnimatePresence>
 
       <AnimatePresence>
-        {status === 'boss_attack' && (
-          <motion.div layout initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: -10 }} exit={{ opacity: 0, y: -60 }} transition={{ ease: 'easeInOut', duration: 0.2 }}>
-            <Button onClick={handleBossAttack} error disabled={!validateDiscard}>
+        {status === "boss_attack" && (
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: -10 }}
+            exit={{ opacity: 0, y: -60 }}
+            transition={{ ease: "easeInOut", duration: 0.2 }}
+          >
+            <Button
+              onClick={() => handleCommands("Discard")}
+              error
+              disabled={!validate.discardButton}
+            >
               DISCARD
-              <span className="count"> {discardVal > 0 ? discardVal : ''}</span>
+              <span className="count">
+                {" "}
+                {validate.discardValue > 0 ? validate.discardValue : ""}
+              </span>
             </Button>
           </motion.div>
         )}
       </AnimatePresence>
 
-      {status === 'game_over_win' && (
+      {status === "game_over_win" && (
         <>
           <div className="game-over">YOU WON</div>
           <Link to="/">
@@ -47,7 +76,7 @@ const Status = (props) => {
         </>
       )}
 
-      {status === 'game_over_lose' && (
+      {status === "game_over_lose" && (
         <>
           <div className="game-over">YOU LOSE</div>
           <Link to="/">
