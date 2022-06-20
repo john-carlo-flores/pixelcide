185c0c03e0792572ab05cb24eaf53ba814974520
JC Flores <tothenextcode@gmail.com>
2022-06-20 15:06:46 -0700

    Fix rendering of boss with new object sctructure

--- frontend/src/components/Game/Deck.jsx
+++ frontend/src/components/Game/Deck.jsx
@@ -1,15 +1,18 @@
-import Card from '../Game/Card';
-import emptyCard from '../../assets/cards/placeholder.png';
-import '../../styles/Game/Deck.scss';
+import Card from "../Game/Card";
+import emptyCard from "../../assets/cards/placeholder.png";
+import "../../styles/Game/Deck.scss";
 
 const Deck = (props) => {
-  const { deck, name, currentBoss } = props;
+  const { deck, name, boss } = props;
 
   //waiting on the boss
-  const theBoss = currentBoss || {};
+  const bossCard = boss?.current;
+  const bossStats = boss?.stats;
 
-  const lastDiscardCard = deck.length > 0 && name === 'discard' ? deck.at(-1) : {};
-  const lastTavernCard = deck.length > 0 && name === 'tavern' ? deck.at(-1) : {};
+  const lastDiscardCard =
+    deck.length > 0 && name === "discard" ? deck.at(-1) : {};
+  const lastTavernCard =
+    deck.length > 0 && name === "tavern" ? deck.at(-1) : {};
 
   return (
     <div className="Deck">
@@ -18,23 +21,23 @@ const Deck = (props) => {
 
       {/* discard deck */}
       <div className="deckName">{name.toUpperCase()}</div>
-      <div className={deck.length && 'deck-main'}>
+      <div className={deck.length && "deck-main"}>
         {/* {name === 'discard' && !deck.length ? <Card image={emptyCard} /> : <Card image={lastDiscardCard.image_front} />} */}
-        {name === 'discard' && <Card image={lastDiscardCard.image_front} />}
+        {name === "discard" && <Card image={lastDiscardCard.image_front} />}
 
         {/* castle deck */}
-        {name === 'castle' && <Card image={theBoss.image_front} />}
+        {name === "castle" && <Card image={bossCard.image_front} />}
 
         {/* tavern deck */}
-        {name === 'tavern' && <Card image={lastTavernCard.image_back} />}
+        {name === "tavern" && <Card image={lastTavernCard.image_back} />}
       </div>
 
       {/* show boss stats */}
-      {name === 'castle' && (
+      {name === "castle" && (
         <div className="nes-container with-title is-centered">
           <p className="title">Enemy status</p>
-          <p className="boss-stats">Attack: {theBoss.damage}</p>
-          <p className="boss-stats">Health: {theBoss.health}</p>
+          <p className="boss-stats">Attack: {bossStats.damage}</p>
+          <p className="boss-stats">Health: {bossStats.health}</p>
         </div>
       )}
     </div>
--- frontend/src/components/Game/DeckList.jsx
+++ frontend/src/components/Game/DeckList.jsx
@@ -1,13 +1,13 @@
-import Deck from './Deck';
-import '../../styles/Game/DeckList.scss';
+import Deck from "./Deck";
+import "../../styles/Game/DeckList.scss";
 
 const DeckList = (props) => {
-  const { tavern, discard, castle, currentBoss } = props;
+  const { tavern, discard, castle, boss } = props;
   return (
     <div className="DeckList">
-      {<Deck deck={discard} name={'discard'} />}
-      {<Deck deck={castle} name={'castle'} currentBoss={currentBoss} />}
-      {<Deck deck={tavern} name={'tavern'} />}
+      {<Deck deck={discard} name={"discard"} />}
+      {<Deck deck={castle} name={"castle"} boss={boss} />}
+      {<Deck deck={tavern} name={"tavern"} />}
     </div>
   );
 };
