62010dba7eb55997a87234ee1a8fdbd16d86d343
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-21 15:04:58 -0500

    fix flex-container moving on removing cards from playerhand

--- frontend/src/components/Game/PlayerHand.jsx
+++ frontend/src/components/Game/PlayerHand.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 import { playable } from "../../helpers/player-helpers";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const PlayerHand = (props) => {
   const { playerHand, playerField, status, moveCardTo, playOn, playerTurn } = props;
@@ -28,13 +28,21 @@ const PlayerHand = (props) => {
   };
 
   const playerHandList = playerHand.map((card) => (
-    <div
-      key={card.id}
-      onClick={() => onClick(card)}
-      className={(status === "player_attack" && playable(card, playerField)) || status === "boss_attack" ? "player-card highlight nes-pointer" : "player-card dull"}
-    >
-      <Card image={card.image_front} />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        transition={{ ease: "easeIn", duration: 0.4 }}
+        whileHover={{
+          y: playableStatus(card) && -15,
+          transition: { duration: 0 },
+          x: playableStatus(card) && -20,
+        }}
+        key={card.id}
+        onClick={() => onClick(card)}
+        className={(status === "player_attack" && playable(card, playerField)) || status === "boss_attack" ? "player-card highlight nes-pointer" : "player-card dull"}
+      >
+        <Card image={card.image_front} />
+      </motion.div>
+    </AnimatePresence>
   ));
 
   return <div className="cards-container">{playerHandList}</div>;
