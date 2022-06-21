7e6065e59dc073410e6400a2591ef798a52929e1
JC Flores <tothenextcode@gmail.com>
2022-06-21 07:07:18 -0700

    Add Mouse Up and Down listeners

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,17 +1,22 @@
-import classNames from 'classnames';
+import classNames from "classnames";
 
 export default function Button(props) {
   let buttonClass = classNames(
-    'nes-btn',
-    { 'is-primary': props.primary },
-    { 'is-success': props.success },
-    { 'is-warning': props.warning },
-    { 'is-error': props.error },
-    { 'is-disabled': props.disabled }
+    "nes-btn",
+    { "is-primary": props.primary },
+    { "is-success": props.success },
+    { "is-warning": props.warning },
+    { "is-error": props.error },
+    { "is-disabled": props.disabled }
   );
 
   return (
-    <button onClick={props.onClick} className={buttonClass}>
+    <button
+      onClick={props.onClick}
+      className={buttonClass}
+      onMouseDown={props.onMouseDown}
+      onMouseUp={props.onMouseUp}
+    >
       {props.children}
     </button>
   );
