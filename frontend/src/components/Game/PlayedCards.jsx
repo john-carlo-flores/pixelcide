790b30567aa9e64d57194c4fdc059a732fe625e1
JC Flores <tothenextcode@gmail.com>
2022-06-21 07:08:28 -0700

    Remove played cards title and absolute position

--- frontend/src/components/Game/PlayedCards.jsx
+++ frontend/src/components/Game/PlayedCards.jsx
@@ -1,19 +1,16 @@
-import '../../styles/Game/PlayedCards.scss';
-import Card from './Card';
+import "../../styles/Game/PlayedCards.scss";
+import Card from "./Card";
 
 const PlayedCards = (props) => {
   const { playedCards } = props;
 
   return (
-    <div className="played-card-container">
-      <div className="deckName">PLAYED CARDS</div>
-      <div className="PlayedCards">
-        {playedCards.map((card) => (
-          <div key={card.id} className="player-card">
-            <Card image={card.image_front} name={card.card_name} />
-          </div>
-        ))}
-      </div>
+    <div className="PlayedCards">
+      {playedCards.map((card) => (
+        <div key={card.id} className="player-card">
+          <Card image={card.image_front} name={card.card_name} />
+        </div>
+      ))}
     </div>
   );
 };
