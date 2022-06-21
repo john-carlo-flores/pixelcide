6a9073d374525e031c53dcbefa8a0e177a29dcc6
JC Flores <tothenextcode@gmail.com>
2022-06-21 16:05:33 -0700

    Remove test router

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -16,19 +16,71 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Router() {
   const socket = useContext(SocketContext);
-  const { user, verifyLogin, logout, register, updateUserAvatar } = useAuth(socket);
+  const { user, verifyLogin, logout, register, updateUserAvatar } =
+    useAuth(socket);
   const state = useLobby(socket);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage userAuth={verifyLogin} logout={logout} user={user} state={state} updateUserAvatar={updateUserAvatar} />} />
-        <Route path="games" element={<Games userAuth={verifyLogin} logout={logout} user={user} updateUserAvatar={updateUserAvatar} />} />
-        <Route path="games/:id" element={<GameRoom userAuth={verifyLogin} logout={logout} user={user} state={state} updateUserAvatar={updateUserAvatar} />} />
-        <Route path="leaderboard" element={<Leaderboard userAuth={verifyLogin} logout={logout} user={user} updateUserAvatar={updateUserAvatar} />} />
-        <Route path="statistics" element={<Statistics userAuth={verifyLogin} logout={logout} user={user} updateUserAvatar={updateUserAvatar} />} />
+        <Route
+          path="/"
+          element={
+            <Homepage
+              userAuth={verifyLogin}
+              logout={logout}
+              user={user}
+              state={state}
+              updateUserAvatar={updateUserAvatar}
+            />
+          }
+        />
+        <Route
+          path="games"
+          element={
+            <Games
+              userAuth={verifyLogin}
+              logout={logout}
+              user={user}
+              updateUserAvatar={updateUserAvatar}
+            />
+          }
+        />
+        <Route
+          path="games/:id"
+          element={
+            <GameRoom
+              userAuth={verifyLogin}
+              logout={logout}
+              user={user}
+              state={state}
+              updateUserAvatar={updateUserAvatar}
+            />
+          }
+        />
+        <Route
+          path="leaderboard"
+          element={
+            <Leaderboard
+              userAuth={verifyLogin}
+              logout={logout}
+              user={user}
+              updateUserAvatar={updateUserAvatar}
+            />
+          }
+        />
+        <Route
+          path="statistics"
+          element={
+            <Statistics
+              userAuth={verifyLogin}
+              logout={logout}
+              user={user}
+              updateUserAvatar={updateUserAvatar}
+            />
+          }
+        />
         <Route path="signup" element={<Registration onSubmit={register} />} />
-        <Route path="test" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
