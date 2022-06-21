d937e17412598c80b4a0e13143ea4f49ceba5fd3
JC Flores <tothenextcode@gmail.com>
2022-06-21 07:13:47 -0700

    Implement Mouse hold to view played cards component

--- frontend/src/components/Game/PlayerList.jsx
+++ frontend/src/components/Game/PlayerList.jsx
@@ -1,26 +1,57 @@
 import Player from "./Player";
+import Button from "../Button";
+
+import { useState } from "react";
+
+import { FaRegEye } from "react-icons/fa";
+import "../../styles/Game/PlayerList.scss";
 
 const PlayerList = (props) => {
-  const { players, user, moveCardTo, status, bossSuit } = props;
+  const { players, user, moveCardTo, status, bossSuit, currentPlayer } = props;
+  const [viewPlayed, setViewPlayed] = useState(false);
 
   const playerList = players.map((player) => {
+    console.log(`Player ${player.username} [${player.id}]`);
+    console.log(
+      `currentPlayer ${currentPlayer.username} [${currentPlayer.id}]`
+    );
+    console.log(`Comparison ${currentPlayer.id === player.id}`);
+
     return (
-      <Player
-        key={player.id}
-        playerField={player.field}
-        playerHand={player.hand}
-        playerDiscard={player.discard}
-        playedCards={player.played}
-        playerName={player.username}
-        avatar={user.avatar_id}
-        status={status}
-        bossSuit={bossSuit}
-        moveCardTo={moveCardTo}
-      />
+      <div className="PlayerList">
+        <Player
+          key={player.id}
+          playerField={player.field}
+          playerHand={player.hand}
+          playerDiscard={player.discard}
+          playedCards={player.played}
+          playerName={player.username}
+          avatar={user.avatar_id}
+          status={status}
+          bossSuit={bossSuit}
+          moveCardTo={moveCardTo}
+          viewPlayed={viewPlayed}
+          owner={user.id === player.id}
+          playerTurn={currentPlayer.id === player.id}
+        />
+      </div>
     );
   });
 
-  return <div className="PlayerList">{playerList}</div>;
+  return (
+    <div>
+      <div className="view-played">
+        <Button
+          onMouseDown={() => setViewPlayed(true)}
+          onMouseUp={() => setViewPlayed(false)}
+          warning
+        >
+          <FaRegEye size={25} />
+        </Button>
+      </div>
+      <div className="PlayerList">{playerList}</div>
+    </div>
+  );
 };
 
 export default PlayerList;
