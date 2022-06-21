70ffd5e77e5f8b26ebebc4bc163bab23f75661c9
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-21 18:37:14 -0500

    add avatars to leaderboard

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -5,15 +5,15 @@ module.exports = (db) => {
     const command = `WITH scores as
     (
     select rank() over(order by sum(case when status = 'WIN' then 1 else 0 end) DESC, sum(case when status = 'WIN' then 1 else 0 end)::FLOAT / count(*)::FLOAT DESC, sum(number_of_moves) ASC) as rank,
-    u.username,
-    sum(case when status = 'WIN' then 1 else 0 end) as total_wins,
+    u.username, u.avatar_id,
+    sum(case when status = 'WIN' then 1 else 0 end) as total_wins, 
     count(*) as total_games,
     (sum(case when status = 'WIN' then 1 else 0 end)::FLOAT / count(*)::FLOAT) * 100 as win_percentage,
     sum(number_of_moves) as total_moves
     from user_games ug
     inner join games g on ug.game_id = g.id
     inner join users u on ug.user_id = u.id
-    group by u.username
+    group by u.username, u.avatar_id
     )
     select * 
     from scores where rank <=10;`;
