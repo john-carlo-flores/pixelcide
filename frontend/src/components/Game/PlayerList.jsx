fd148cd833200e70caf7b9bdc99941e73c3391a7
JC Flores <tothenextcode@gmail.com>
2022-06-21 13:49:44 -0700

    Pass off handleCommands to PlayersList

--- frontend/src/components/Game/PlayerList.jsx
+++ frontend/src/components/Game/PlayerList.jsx
@@ -2,24 +2,28 @@ import Player from "./Player";
 import "../../styles/Game/PlayerList.scss";
 
 const PlayerList = (props) => {
-  const { players, user, moveCardTo, status, bossSuit, currentPlayer } = props;
+  const {
+    players,
+    user,
+    moveCardTo,
+    status,
+    bossSuit,
+    currentPlayer,
+    handleCommands,
+  } = props;
 
   const playerList = players.map((player) => {
     return (
       <div className="PlayerList">
         <Player
           key={player.id}
-          playerField={player.field}
-          playerHand={player.hand}
-          playerDiscard={player.discard}
-          playedCards={player.played}
-          playerName={player.username}
-          avatar={user.avatar_id}
+          player={player}
           status={status}
           bossSuit={bossSuit}
           moveCardTo={moveCardTo}
           owner={user.id === player.id}
           playerTurn={currentPlayer.id === player.id}
+          handleCommands={handleCommands}
         />
       </div>
     );
