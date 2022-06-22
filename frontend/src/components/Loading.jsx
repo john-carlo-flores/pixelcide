736bce508641388817514c08ff2abd10ae8b6e95
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-06-22 00:46:33 -0500

    add styles to loading screen

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,37 +1,36 @@
-import loading from '../assets/loading/card-spinner.gif';
+import loading from "../assets/loading/card-spinner.gif";
 
-import styles from '../styles/Loading.module.scss';
+import styles from "../styles/Loading.module.scss";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Loading = () => {
-  const [dots, setDots] = useState('Loading Game');
+  const [dots, setDots] = useState("Loading Game");
 
   const cycleDots = () => {
-    setDots(prev => {
+    setDots((prev) => {
       if (prev.length < 15) {
         return `${prev}.`;
       }
 
-      return 'Loading Game';
+      return "Loading Game";
     });
   };
 
   useEffect(() => {
     const timer = setInterval(() => {
       cycleDots();
-    }, 500);
+    }, 100);
 
     return () => clearInterval(timer);
   }, []);
 
   return (
     <div className={styles.loading}>
-      <img className={styles.img} src={loading} alt='Loading'/>
-      <span className={styles.loadingText}>{dots}</span>
+      <img className={styles.img} src={loading} alt="Loading" />
+      {/* <span className={styles.loadingText}>{dots}</span> */}
     </div>
   );
 };
 
 export default Loading;
-
