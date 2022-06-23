2df02cc0763be3382c2dcb4ffc45ba7698ae1c7f
JC Flores <tothenextcode@gmail.com>
2022-06-23 00:47:21 -0700

    Add randomizer avatar id on register

--- frontend/src/components/Authentication/Registration.jsx
+++ frontend/src/components/Authentication/Registration.jsx
@@ -2,8 +2,12 @@ import Button from "../Button";
 
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaGithubAlt } from 'react-icons/fa';
-import { GiRetroController, GiAttachedShield, GiFragmentedSword } from 'react-icons/gi';
+import { FaGithubAlt } from "react-icons/fa";
+import {
+  GiRetroController,
+  GiAttachedShield,
+  GiFragmentedSword,
+} from "react-icons/gi";
 import { ImPacman } from "react-icons/im";
 
 import styles from "../../styles/Authentication/Registration.module.scss";
@@ -31,8 +35,8 @@ const Registration = (props) => {
       name,
       email,
       password,
-      avatar_id: 1
-    }
+      avatar_id: Math.floor(Math.random() * 13),
+    };
 
     onSubmit(user).then((result) => {
       navigate("/");
@@ -46,29 +50,71 @@ const Registration = (props) => {
       <div className={`${styles.form} nes-container is-rounded`}>
         <form onSubmit={confirmRegistration}>
           <div className="nes-field is-inline">
-            <label><FaGithubAlt size={70}/></label>
-            <input className="nes-input" type="text" name="uname" placeholder="Username" required />
+            <label>
+              <FaGithubAlt size={70} />
+            </label>
+            <input
+              className="nes-input"
+              type="text"
+              name="uname"
+              placeholder="Username"
+              required
+            />
           </div>
           <div className="nes-field is-inline">
-            <label><ImPacman size={70}/></label>
-            <input className="nes-input" type="text" name="name" placeholder="Name" required />
+            <label>
+              <ImPacman size={70} />
+            </label>
+            <input
+              className="nes-input"
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+            />
           </div>
           <div className="nes-field is-inline">
-            <label><GiRetroController size={70}/></label>
-            <input className="nes-input" type="email" name="email" placeholder="Email Address" required />
+            <label>
+              <GiRetroController size={70} />
+            </label>
+            <input
+              className="nes-input"
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
           </div>
           <div className="nes-field is-inline">
-            <label><GiFragmentedSword size={70}/></label>
-            <input className="nes-input" type="password" name="pass" placeholder="Password" required />
+            <label>
+              <GiFragmentedSword size={70} />
+            </label>
+            <input
+              className="nes-input"
+              type="password"
+              name="pass"
+              placeholder="Password"
+              required
+            />
           </div>
           <div className="nes-field is-inline">
-            <label><GiAttachedShield size={70}/></label>
-            <input className="nes-input" type="password" name="passconf" placeholder="Password Confirmation" required />
+            <label>
+              <GiAttachedShield size={70} />
+            </label>
+            <input
+              className="nes-input"
+              type="password"
+              name="passconf"
+              placeholder="Password Confirmation"
+              required
+            />
           </div>
           {error && <p>{error}</p>}
           <div className="button-container">
             <Button success>Register</Button>
-            <Link to="/"><Button>Cancel</Button></Link>
+            <Link to="/">
+              <Button>Cancel</Button>
+            </Link>
           </div>
         </form>
       </div>
